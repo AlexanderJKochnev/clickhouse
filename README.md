9417fb6a2b0cc8044c6b5dd2f902f0e00285ec82
Alexander <akochnev66@gmail.com>
2026-03-26 16:32:11 +0300

    first commit  Изменения, которые будут включены в коммит: 	новый файл:    .idea/.gitignore 	новый файл:    .idea/copyright/AlexKochnev.xml 	новый файл:    .idea/dictionaries 	новый файл:    .idea/misc.xml 	новый файл:    .idea/vcs.xml 	изменено:      README.md 	новый файл:    ch_init/01_sync.sql 	новый файл:    ch_init/02_sync.sql 	новый файл:    docker-compose.yaml

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # clickhouse
 clickhouse_repo
+
+
+1. ALTER USER wine_user WITH REPLICATION;  #  выполнить в postgresql если пользователь не имеет прав на реплики
---
+++ ch_init/02_sync.sql
@@ -0,0 +1,27 @@
+-- 1. Подключаемся к Postgres (Внешняя таблица)
+CREATE TABLE IF NOT EXISTS remote_items_stream (
+    id Int32,
+    search_content String
+) ENGINE = PostgreSQL('wine_host:5432', 'wine_db', 'items', 'wine', 'wine1');
+
+-- 2. Локальная таблица для быстрого поиска
+-- Используем Inverted индекс для ускорения полнотекстового поиска
+CREATE TABLE IF NOT EXISTS local_items (
+    id Int32,
+    search_content String,
+    updated_at DateTime DEFAULT now(),
+    -- Индекс для быстрого поиска по словам (аналог GIN в Postgres)
+    INDEX inv_idx search_content TYPE inverted(0) GRANULARITY 1
+) ENGINE = MergeTree()
+ORDER BY id;
+
+CREATE DATABASE IF NOT EXISTS wine_replica
+ENGINE = MaterializedPostgreSQL(
+    'wine_host:5432',
+    'wine_db',
+    'wine',
+    'wine1'
+)
+SETTINGS
+    materialized_postgresql_tables_list = 'items',
+    materialized_postgresql_allow_automatic_update = 1;
